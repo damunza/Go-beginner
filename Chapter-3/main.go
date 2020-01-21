1f57f8b31535eeac2a1a9da7b348f88df5de0ac6
damunza <danidamunza@gmail.com>
2020-01-21 17:12:39 +0300

    figured out multi function running

--- Chapter-3/main.go
+++ Chapter-3/main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 func main(){
+	/*
+	this function is used to call 
+	everything that should run when the program is launched 
+	*/
+	farenCel()
+	meterFeet()
+
+}
+
+func farenCel(){
 	fmt.Println("Enter temperature in degrees farenheit: ")
 	var input float64
 	fmt.Scanf("%f", &input)
@@ -9,4 +19,13 @@ func main(){
 	output:= ((input-32) *5/9) // remember that divisions and multiplications dont need brackets
 	fmt.Println(output)
 
+}
+
+func meterFeet(){
+	fmt.Println("Feet to convert to meters:")
+	var input float64
+	fmt.Scanf("%f", &input)
+
+	output:= (input/0.3048)
+	fmt.Println(output)
 }

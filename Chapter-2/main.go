9ab8999b3f97c219ac64f69bec4b47e21c289507
damunza <danidamunza@gmail.com>
2020-01-21 17:13:20 +0300

    data types

---
+++ Chapter-2/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+func main(){
+	fmt.Println("1 + 1 =", 1+1)
+	fmt.Println(len("Hello , World"))
+	fmt.Println("hello, world"[2])
+	fmt.Println("hello world, \n" + "its Dan")
+	fmt.Println(!false&&false) // means not false and true 
+}

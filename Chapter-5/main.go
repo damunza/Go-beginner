ff74a648a271cecfd5270c889e355dc668628f70
damunza <danidamunza@gmail.com>
2020-01-22 17:08:47 +0300

    complex data types

--- Chapter-5/main.go
+++ Chapter-5/main.go
@@ -2,29 +2,29 @@ package main
 
 import "fmt"
 func main(){
-	// mean()
+	mean()
 	dictionary()
 }
 
-// if you dont include the float the valuess will be rounded down
+// if you dont include the float the values will be rounded down
 
-// func mean(){
-// 	x := [5]float64{
-// 		90,
-// 		96,
-// 		80,
-// 		84,
-// 		78,
-// 	}
-// 	/*when declaring numerical values that need a 
-// 	math operation type is declared as a float
-// 	*/
-// 	total:=0.0 // add the .0 to create a float automatically
-// 	for _, value := range x{
-// 		total += value
-// 	}
-// 	fmt.Println(total/float64(len(x)))
-// }
+func mean(){
+	x := [5]float64{
+		90,
+		96,
+		80,
+		84,
+		78,
+	}
+	/*when declaring numerical values that need a 
+	math operation type is declared as a float
+	*/
+	total:=0.0 // add the .0 to create a float automatically
+	for _, value := range x{
+		total += value
+	}
+	fmt.Println(total/float64(len(x)))
+}
 
 /*
 - dealing with map< dictionaries> data types 

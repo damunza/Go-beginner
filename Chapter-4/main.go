24ee9995adb3808437cb7d4cd14fd524869270ec
damunza <danidamunza@gmail.com>
2020-01-22 13:11:14 +0300

    discovered statements for shared variables should start separately for each equqtion

--- Chapter-4/main.go
+++ Chapter-4/main.go
@@ -2,15 +2,39 @@ package main
 
 import "fmt"
 func main(){
-	lessTen()
+	// lessTen()
+	// multiplesThree()
+	fizzBuzz()
 }
 
 /* unlike in python loops are bound by curly 
 bracets not indenteds
 */
 
-func lessTen(){
-	for i:=1; i<=10; i++{
-		fmt.Println(i)
+// func lessTen(){
+// 	for i:=1; i<=10; i++{
+// 		fmt.Println(i)
+// 	}
+// }
+
+// func multiplesThree(){
+// 	for i:=1; i <=100; i++{
+// 	if i % 3 == 0{
+// 		fmt.Println(i)
+// 	}
+// }
+// }
+
+func fizzBuzz(){
+	for i:=1; i <= 100; i++{
+		if i % 3 == 0{
+			fmt.Println("Fizz")
+		}
+		if i % 5 ==0{
+			fmt.Println(" Buzz")
+		}
+		if i %3 != 0 && i%5 != 0{
+			fmt.Println(i)	
+		}
 	}
 }

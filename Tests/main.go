4d6e6e7426834d4994e6ae8ab79ac7f6d0193bbc
damunza <danidamunza@gmail.com>
2020-01-23 14:44:35 +0300

    do not declare a variable when replacing an undefined variable creates a not used error

--- Tests/main.go
+++ Tests/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 func main(){
 	swap()
+	largest()
 }
 
 func change(x, y * int){
@@ -13,4 +14,29 @@ func swap(){
 	a, b := 1, 2
 	change(&a, &b)
 	fmt.Println(a,b)
+}
+
+func big(x ...int)int{
+	var current_no int
+	for _,item := range x {
+		// starting from my own value 0 
+		// item equal to 0 check if variable larger than mine
+		// if so change mine to the larger number
+		if current_no == 0 || item > current_no{
+			/*
+			or is uded because it returns the correct statement
+			of the two .
+			assigns current number 0 then checks if the position
+			iterated is larger than the current number returns 
+			the true statement of the two
+			*/
+			current_no = item
+		}
+		
+	}
+	return current_no
+}
+
+func largest(){
+	fmt.Println(big(1,5,3,4,9))
 }
